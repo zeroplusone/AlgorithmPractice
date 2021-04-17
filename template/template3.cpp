09b823516304b31c35ce3708acb4de4f3349d9d9
zeroplusone <a711186@gmail.com>
2021-04-17 16:54:06 +0800

    add new template

---
+++ template/template3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define LL long long
+#define PR pair<int,int>
+
+#define pb push_back
+#define mp make_pair
+#define x first
+#define y second
+
+int main()
+{
+    int T; scanf("%d", &T);
+    for(int tt = 1; tt <= T; tt++){
+        printf("Case #%d: ", tt);
+        printf("Answer\n");
+    }
+
+    for (int i = 0; i <; ++i) {
+
+    }
+
+}
