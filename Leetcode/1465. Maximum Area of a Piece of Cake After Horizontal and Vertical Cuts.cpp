5d7e7dc0a0847bd667a2ed57463e59ded46d4707
LIN CHIA-YING <zeroplusone@users.noreply.github.com>
2021-06-03 21:17:28 +0800

    Update 1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp

--- Leetcode/1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
+++ Leetcode/1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
@@ -3,31 +3,16 @@ public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
         sort(horizontalCuts.begin(), horizontalCuts.end());
         sort(verticalCuts.begin(), verticalCuts.end());
-        long long int maxW=0, maxH=0;
-        for(int i=0;i<=horizontalCuts.size();++i) {
-            long long int diff;
-            if(i==0) {
-                diff=horizontalCuts[i];
-            } else if(i==horizontalCuts.size()) {
-                diff=h-horizontalCuts[i-1];
-            } else {
-                diff=horizontalCuts[i]-horizontalCuts[i-1];
-            }
-            maxH=max(diff, maxH);
+        int maxH=max(horizontalCuts[0], h-horizontalCuts[horizontalCuts.size()-1]);
+        int maxW=max(verticalCuts[0], w-verticalCuts[verticalCuts.size()-1]);
+        for(int i=1;i<horizontalCuts.size();++i) {
+            maxH=max(maxH, horizontalCuts[i]-horizontalCuts[i-1]);
         }
-        for(int i=0;i<=verticalCuts.size();++i) {
-            long long int diff;
-            if(i==0) {
-                diff=verticalCuts[i];
-            } else if(i==verticalCuts.size()) {
-                diff=w-verticalCuts[i-1];
-            } else {
-                diff=verticalCuts[i]-verticalCuts[i-1];
-            }
-            maxW=max(diff, maxW);
+        for(int i=1;i<verticalCuts.size();++i) {
+            maxW=max(maxW, verticalCuts[i]-verticalCuts[i-1]);
         }
         long long int mod = 1e9+7;
-        long long int ans = maxW*maxH%mod;
+        long long int ans = (long long int)maxW*(long long int)maxH%mod;
         return ans;
     }
 };
