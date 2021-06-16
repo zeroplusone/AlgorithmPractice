49951fbea5052ef3143abd252175091e260ae555
LIN CHIA-YING <zeroplusone@users.noreply.github.com>
2021-06-16 21:28:55 +0800

    Update 22. Generate Parentheses.cpp

--- Leetcode/22. Generate Parentheses.cpp
+++ Leetcode/22. Generate Parentheses.cpp
@@ -5,14 +5,6 @@ public:
         dp[0]=unordered_set<string>{""};
         dp[1]=unordered_set<string>{"()"};
         for(int i=2;i<=n;++i) {
-            string init="";
-            for(int j=0;j<i;++j) {
-                init+="(";
-            }
-            for(int j=0;j<i;++j) {
-                init+=")";
-            }
-            dp[i].insert(init);
             for(int j=1;j<i;++j) {
                 for(auto k: dp[j]) {
                     for(auto l: dp[i-j]) {
@@ -25,7 +17,6 @@ public:
             }
         }
 
-        vector<string> ans = vector<string>(dp[n].begin(), dp[n].end());
-        return ans;
+        return vector<string>(dp[n].begin(), dp[n].end());
     }
 };
