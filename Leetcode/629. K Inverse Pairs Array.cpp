6380005553962c5a0698520a0b5e7d1c14f01f81
LIN CHIA-YING <zeroplusone@users.noreply.github.com>
2021-06-19 22:40:47 +0800

    Update 629. K Inverse Pairs Array.cpp

--- Leetcode/629. K Inverse Pairs Array.cpp
+++ Leetcode/629. K Inverse Pairs Array.cpp
@@ -4,24 +4,25 @@ public:
         if(k==0) {
             return 1;
         }
-        vector<vector<long long int>> dp(n+1, vector<long long int>(k+1, 0));
+        vector<vector<long long int>> dp(2, vector<long long int>(k+1, 0));
         int mod=1e9+7;
 
-        dp[1][0]=1;
+        dp[0][0]=1;
         int len=1;
         for(int i=2;i<=n;++i) {
             long long int sum=0;
             len=i-1+len; 
             for(int j=0;j<=min(k, len/2);++j) {
-                sum=(sum+dp[i-1][j])%mod;
-                sum=(sum-(j>=i?dp[i-1][j-i]:0)+mod)%mod;
-                dp[i][j]=sum;
+                sum=(sum+dp[0][j])%mod;
+                sum=(sum-(j>=i?dp[0][j-i]:0)+mod)%mod;
+                dp[1][j]=sum;
                 if(len-j-1<=k) {
-                    dp[i][len-j-1]=sum;
+                    dp[1][len-j-1]=sum;
                 }
             }
+            dp[0]=dp[1];
         }
-        return dp[n][k]%mod;
+        return dp[0][k]%mod;
     }
     
 };
