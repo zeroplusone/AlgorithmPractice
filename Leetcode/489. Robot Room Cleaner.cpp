a63e444e8076ae69ade61f369c72aebb4ecc1926
LIN CHIA-YING <zeroplusone@users.noreply.github.com>
2021-08-07 01:09:39 +0800

    Update 489. Robot Room Cleaner.cpp

--- Leetcode/489. Robot Room Cleaner.cpp
+++ Leetcode/489. Robot Room Cleaner.cpp
@@ -19,199 +19,35 @@
 
 class Solution {
 public:
+
+    vector<int> diffx=vector<int>{0, -1, 0, 1};
+    vector<int> diffy=vector<int>{1, 0, -1, 0};
     void cleanRoom(Robot& robot) {
-        unordered_set<string> v;
+        unordered_map<int, unordered_map<int, int>> v;
         DFS(0, 0, 0, robot, v);
     }
     
-    void DFS(int x, int y, int dir, Robot& robot, unordered_set<string>& v) {
-        v.insert(hash(x, y));
+    void DFS(int x, int y, int dir, Robot& robot, unordered_map<int, unordered_map<int, int>>& v) {
+        v[x][y]=true;
         robot.clean();
         
         int newx, newy, newdir;
         // 0 up, 1 left, 2 down, 3 right
-        robot.turnRight();
-        newx=calx(x, dir, 3);
-        newy=caly(y, dir, 3);
-        newdir=caldir(dir, 3);
-        if(v.count(hash(newx, newy))==0 && robot.move()) {
-            DFS(newx, newy, newdir, robot, v);
-        }
-        
-        robot.turnLeft();
-        newx=calx(x, dir, 0);
-        newy=caly(y, dir, 0);
-        newdir=caldir(dir, 0);
-        if(v.count(hash(newx, newy))==0 && robot.move()) {
-            DFS(newx, newy, newdir, robot, v);
-        }
-        
-        robot.turnLeft();
-        newx=calx(x, dir, 1);
-        newy=caly(y, dir, 1);
-        newdir=caldir(dir, 1);
-        if(v.count(hash(newx, newy))==0 && robot.move()) {
-            DFS(newx, newy, newdir, robot, v);
-        }
-        
-        robot.turnLeft();
-        newx=calx(x, dir, 2);
-        newy=caly(y, dir, 2);
-        newdir=caldir(dir, 2);
-        if(v.count(hash(newx, newy))==0 && robot.move()) {
-            DFS(newx, newy, newdir, robot, v);
+        for(int i=3;i>=0;--i) {
+            robot.turnRight();
+            newx=x+diffx[(i+dir)%4];
+            newy=y+diffy[(i+dir)%4];
+            newdir=(dir+i)%4;
+            if(!v[newx][newy] && robot.move()) {
+                DFS(newx, newy, newdir, robot, v);
+            }
         }
 
+        robot.turnRight();
+        robot.turnRight();
         robot.move();
         robot.turnRight();
         robot.turnRight();
     }
-    
-    int caldir(int dir, int turn) {
-        // 0 up, 1 left, 2 down, 3 right
-        switch(dir){ // face
-            // move
-            case 0:
-                if(turn==0) {
-                    return 0;
-                } else if(turn==1) {
-                    return 1;
-                } else if(turn==2) {
-                    return 2;
-                } else if(turn==3) {
-                    return 3;
-                }
-            case 1:
-                if(turn==0) {
-                    return 1;
-                } else if(turn==1) {
-                    return 2;
-                } else if(turn==2) {
-                    return 3;
-                } else if(turn==3) {
-                    return 0;
-                }
-            case 2:
-                if(turn==0) {
-                    return 2;
-                } else if(turn==1) {
-                    return 3;
-                } else if(turn==2) {
-                    return 0;
-                } else if(turn==3) {
-                    return 1;
-                }
-            case 3:
-                if(turn==0) {
-                    return 3;
-                } else if(turn==1) {
-                    return 0;
-                } else if(turn==2) {
-                    return 1;
-                } else if(turn==3) {
-                    return 2;
-                }
-        }
-        return dir;
-    }
-    
-    int calx(int x, int dir, int turn) {
-        // 0 up, 1 left, 2 down, 3 right
-        switch(dir){ // face
-            // move
-            case 0:
-                if(turn==0) {
-                    return x;
-                } else if(turn==1) {
-                    return x-1;
-                } else if(turn==2) {
-                    return x;
-                } else if(turn==3) {
-                    return x+1;
-                }
-            case 1:
-                if(turn==0) {
-                    return x-1;
-                } else if(turn==1) {
-                    return x;
-                } else if(turn==2) {
-                    return x+1;
-                } else if(turn==3) {
-                    return x;
-                }
-            case 2:
-                if(turn==0) {
-                    return x;
-                } else if(turn==1) {
-                    return x+1;
-                } else if(turn==2) {
-                    return x;
-                } else if(turn==3) {
-                    return x-1;
-                }
-            case 3:
-                if(turn==0) {
-                    return x+1;
-                } else if(turn==1) {
-                    return x;
-                } else if(turn==2) {
-                    return x-1;
-                } else if(turn==3) {
-                    return x;
-                }
-        }
-        return x;
-    }
-    
-    int caly(int y, int dir, int turn) {
-        // 0 up, 1 left, 2 down, 3 right
-        switch(dir){ // face
-            // move
-            case 0:
-                if(turn==0) {
-                    return y+1;
-                } else if(turn==1) {
-                    return y;
-                } else if(turn==2) {
-                    return y-1;
-                } else if(turn==3) {
-                    return y;
-                }
-            case 1:
-                if(turn==0) {
-                    return y;
-                } else if(turn==1) {
-                    return y-1;
-                } else if(turn==2) {
-                    return y;
-                } else if(turn==3) {
-                    return y+1;
-                }
-            case 2:
-                if(turn==0) {
-                    return y-1;
-                } else if(turn==1) {
-                    return y;
-                } else if(turn==2) {
-                    return y+1;
-                } else if(turn==3) {
-                    return y;
-                }
-            case 3:
-                if(turn==0) {
-                    return y;
-                } else if(turn==1) {
-                    return y+1;
-                } else if(turn==2) {
-                    return y;
-                } else if(turn==3) {
-                    return y-1;
-                }
-        }
-        return y;
-    }
-    
-    string hash(int x, int y) {
-        return to_string(x) + "#" + to_string(y);
-    }
+
 };
