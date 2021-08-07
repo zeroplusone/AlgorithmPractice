936feff05456829fdde8a1f81605e96cfab1a460
LIN CHIA-YING <zeroplusone@users.noreply.github.com>
2021-08-08 00:29:34 +0800

    Update 1957. Delete Characters to Make Fancy String.cpp

--- Leetcode/1957. Delete Characters to Make Fancy String.cpp
+++ Leetcode/1957. Delete Characters to Make Fancy String.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        char last=s[0];
         int cnt=1;
-        int i=1;
-        string ans="";
-        ans+=last;
-        while(i<s.length()) {
-            if(s[i]==last) {
-                cnt++;
-            } else {
-                cnt=1;
-                last=s[i];
-            }
-            
+        string ans=""; 
+        
+        ans+=s[0];
+        for(int i=1;i<s.length();++i) {
+            cnt=s[i]==s[i-1]? cnt+1:1;
+
             if(cnt<3) {
                 ans+=s[i];
             }
-            i++;
         }
         return ans;
+
     }
 };
