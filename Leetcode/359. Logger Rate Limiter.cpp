89f83334893acb0408275216fea8d6e20f5b9af4
LIN CHIA-YING <zeroplusone@users.noreply.github.com>
2021-08-19 22:55:20 +0800

    Create 359. Logger Rate Limiter.cpp

---
+++ Leetcode/359. Logger Rate Limiter.cpp
@@ -0,0 +1,27 @@
+class Logger {
+public:
+    /** Initialize your data structure here. */
+    Logger() {
+        
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        if(cache.count(message)==0 || timestamp-cache[message]>=10) {
+            cache[message]=timestamp;
+            return true;
+        } 
+        
+        return false;
+    }
+    
+    unordered_map<string, int> cache;
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
