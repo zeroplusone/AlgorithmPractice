594a598b174cb1736951488e6b472ca800ba1a9c
LIN CHIA-YING <zeroplusone@users.noreply.github.com>
2021-09-04 20:21:57 +0800

    Update 834. Sum of Distances in Tree-2.cpp

--- Leetcode/834. Sum of Distances in Tree-2.cpp
+++ Leetcode/834. Sum of Distances in Tree-2.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        vector<int> ans(n, -1);
+        vector<int> ans(n, 0);
         vector<int> cnt(n, 0);
         unordered_map<int, vector<int>> graph;
         for(int i=0;i<edges.size();++i) {
@@ -10,31 +10,26 @@ public:
             graph[edges[i][1]].push_back(edges[i][0]);
         }
         
-        ans[0]=buildTree(0, -1, graph, cnt);
+        buildTree(0, -1, graph, cnt, ans);
         solve(0, -1, graph, cnt, ans, n);
         return ans;
     }
     
-    int buildTree(int now, int parent, unordered_map<int, vector<int>>& graph, vector<int>& cnt) {
-        int nodeNum=1;
-        int subTreeSum=0;
+    void buildTree(int now, int parent, unordered_map<int, vector<int>>& graph, vector<int>& cnt, vector<int>& ans) {
+        cnt[now]++;
         for(int i=0;i<graph[now].size();++i) {
             if(graph[now][i]!=parent) {
-                subTreeSum+=buildTree(graph[now][i], now, graph, cnt);
-                subTreeSum+=cnt[graph[now][i]];
-                nodeNum+=cnt[graph[now][i]];
+                buildTree(graph[now][i], now, graph, cnt, ans);
+                cnt[now]+=cnt[graph[now][i]];
+                ans[now]+=ans[graph[now][i]]+cnt[graph[now][i]];
             }
         }
-        cnt[now]=nodeNum;
-        return subTreeSum;
     }
     
     void solve(int now, int parent, unordered_map<int, vector<int>>& graph, vector<int>& cnt, vector<int>& ans, int& n) {
-        if(ans[now]==-1) {
-            ans[now]=ans[parent]-cnt[now]+n-cnt[now];    
-        }
         for(int i=0;i<graph[now].size();++i) {
             if(graph[now][i]!=parent) {
+                ans[graph[now][i]]=ans[now]-cnt[graph[now][i]]+n-cnt[graph[now][i]];        
                 solve(graph[now][i], now, graph, cnt, ans, n);
             }
         }
